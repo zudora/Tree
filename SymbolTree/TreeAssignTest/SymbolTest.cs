9ed5bf105b22df3cda48af6e7b44eb2646b3f645
zudora <zudora@users.noreply.github.com>
2015-05-21 22:00:27 -0700

    yo dog i heard you like tests

---
+++ SymbolTree/TreeAssignTest/SymbolTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text;
+using SymbolTree;
+
+namespace TreeAssignTest
+{
+    [TestClass]
+    public class SymbolTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create input
+            Dictionary<string, int> valFreq = new Dictionary<string, int>();
+
+            valFreq.Add("D", 12);
+            valFreq.Add("B", 14);
+            valFreq.Add("K", 1);
+            valFreq.Add("C", 12);
+            valFreq.Add("G", 9);
+            valFreq.Add("H", 6);
+            valFreq.Add("I", 4);
+            valFreq.Add("F", 12);
+            valFreq.Add("J", 3);
+            valFreq.Add("A", 20);
+            valFreq.Add("E", 12);
+
+            //submit input to tree builder
+            // Order value/freq pairs and use to build tree         
+            List<treeNode> nodes1 = Program.listBuild(valFreq);            
+            treeNode root = new treeNode();
+            Dictionary<int, treeNode> testTreeDict = Program.treeBuild(nodes1, out root);
+
+            Dictionary<int, string> testSymbolBuild = Program.SymbolBuild(testTreeDict, root);
+
+            Dictionary<int, string> testValues = Program.nodesWithValues(testSymbolBuild, testTreeDict);
+
+            bool prefixMatch = prefixMatchTest(testValues);
+
+            Assert.IsFalse(prefixMatch, "Prefix found");
+
+            testBadPrefixes();
+        }
+        
+        public void testBadPrefixes()
+        {
+            Dictionary<int, string> testValues = new Dictionary<int,string>();
+
+            testValues.Add(0, "0000");
+            testValues.Add(0, "000");
+            testValues.Add(0, "0010");
+            testValues.Add(0, "0100");
+
+            bool prefixMatch = prefixMatchTest(testValues);
+
+            Assert.IsFalse(prefixMatch, "Prefix found");
+
+        }
+
+        public static bool prefixMatchTest (Dictionary<int, string> testSymbols)
+        {
+            //test for prefixes        
+            bool prefixMatch = false;
+
+            foreach (KeyValuePair<int, string> firstKey in testSymbols)
+            {
+                foreach (KeyValuePair<int, string> secondKey in testSymbols)
+                {
+                    if (firstKey.Key != secondKey.Key)
+                    {
+                        if (secondKey.Value.StartsWith(firstKey.Value))
+                        {
+                            prefixMatch = true;
+                        }
+                    }
+                }
+            }
+            return prefixMatch;
+        }    
+    }
+}
